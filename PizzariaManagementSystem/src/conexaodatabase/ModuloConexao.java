bad4330630c879685fa510e7a39e1d921da632c7
Motta360 <lucas-mottateixeira@hotmail.com>
2024-02-27 18:11:34 -0300

    Primeira versão

---
+++ PizzariaManagementSystem/src/conexaodatabase/ModuloConexao.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conexaodatabase;
+import java.sql.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author lucas
+ */
+public class ModuloConexao {
+    public static Connection connector(){
+        Connection conexao ;
+        String Driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/pizzariadatabase";
+        String user = "root";
+        String password = "12325678Lu";
+        
+        try {
+            Class.forName(Driver);
+            conexao = DriverManager.getConnection(url,user,password);
+            return conexao;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+ 
+    }
+    
+    
+}
